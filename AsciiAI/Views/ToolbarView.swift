880546fc5a6ca694cb32cd7296acc00c26250686
Almog Gavra <almog.gavra@gmail.com>
2026-02-26 16:01:35 -0800

    Center toolbar icons and add vertical padding

--- AsciiAI/Views/ToolbarView.swift
+++ AsciiAI/Views/ToolbarView.swift
@@ -4,15 +4,19 @@ struct ToolbarView: View {
     @Bindable var viewModel: EditorViewModel
 
     var body: some View {
-        HStack(spacing: 4) {
-            toolButton(type: .select, icon: "cursorarrow", tooltip: "Select")
-            toolButton(type: .box, icon: "rectangle", tooltip: "Box")
-            toolButton(type: .arrow, icon: "arrow.right", tooltip: "Line")
-            toolButton(type: .text, icon: "textformat", tooltip: "Text")
+        HStack {
+            Spacer()
+            HStack(spacing: 4) {
+                toolButton(type: .select, icon: "cursorarrow", tooltip: "Select")
+                toolButton(type: .box, icon: "rectangle", tooltip: "Box")
+                toolButton(type: .arrow, icon: "arrow.right", tooltip: "Line")
+                toolButton(type: .text, icon: "textformat", tooltip: "Text")
+            }
+            .padding(.vertical, 3)
             Spacer()
         }
         .padding(.horizontal, 8)
-        .frame(height: 28)
+        .frame(height: 34)
         .background(.bar)
     }
 
